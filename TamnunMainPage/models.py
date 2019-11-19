d93282c7a209afb1397ef5466ff377223d0d95d9
Sior Meir <s8038504@iaf.idf.il>
2019-11-19 20:07:06 +0200

    Start work on models. added TODOs

--- TamnunMainPage/models.py
+++ TamnunMainPage/models.py
@@ -10,10 +10,18 @@ class Aircraft(models.Model):
     modelName = models.CharField(max_length=32, name='modelName')
     
     def __str__(self):
-        return f"AIRCRAFT {self.IAFname} ({self.modelName}), TMS : {self.TMS}"
+        return f"AIRCRAFT {self.IAFname} ({self.modelName}) ; TMS : {self.TMS}"
 
 class Item(models.Model):
-    pass
+    itemName = models.CharField(max_length=32, name='itemName')
+    hasLateralData = models.BooleanField(name='hasLateralData')
+    weight = models.FloatField(name='weight')
+    x_cg = models.FloatField(name='x_cg')
+    y_cg = models.FloatField(name='y_cg')
+    # TODO : add is lateral option
+    
+    def __str__(self):
+        return f"ITEM : {self.itemName} ; WEIGHT = {self.weight} ; X-CG  = {self.x_cg} ; Y-CG = {self.y_cg}"
 
 class TailNumber(models.Model):
     pass
