8bacd54d36707128dfa269e42fb4e5a64e5cc597
Sior Meir <siorm2310@gmail.com>
2021-03-06 21:35:04 +0200

    data.json changed to fit context format. zik_view now fitted with collapsables cards

--- TamnunMainPage/static/TamnunMainPage/js/zik_view.js
+++ TamnunMainPage/static/TamnunMainPage/js/zik_view.js
@@ -1,6 +1,25 @@
 class ItemMenu extends HTMLElement {
     constructor() {
         super();
+
+        this.innerHTML = menuItems
+    }
+    // Fires when an instance was inserted into the document
+      connectedCallback() {
     }
 
+    // Fires when an instance was removed from the document
+    disconnectedCallback() {
+    }
+
+    // Fires when an attribute was added, removed, or updated
+    attributeChangedCallback(attrName, oldVal, newVal) {
+    }
+
+    // Fires when an element is moved to a new document
+    adoptedCallback() {
+    }
 }
+
+// Registers custom element
+window.customElements.define('item-menu', ItemMenu);
--- TamnunMainPage/views.py
+++ TamnunMainPage/views.py
@@ -9,43 +9,11 @@ from .queries import ViewQueries
 # import Algorithms.WBCalc as WBCalc
 
 
-class PlatformSelectionView(ListView):
-    """This view refers the user to a Platform selection list, divided by TMS
-
-    Arguments:
-        ListView {class} -- Django's class-based view
-    """
-
-    # view_types: {
-    #     "UAV": UAVView,
-    #     "Fighter": FighterView,
-    #     "CargoAndHelos": CargoView,
-    #     "HeavyCargo": HeavyCargoView,
-    # }
-
-    # TODO: edit this view
-    model = AircraftType
-
-
 def zik_view(request):
     context = {"a": 0, "b": 1}
-    return render(request, "TamnunMainPage/zik_view.html", context=context)
-
-
-def Weight_and_balance_api(request):
-    pass
-
-
-def general_calc_tamnun(request, methods=["POST", "GET"]):
-    """
-    TODO:
-    1. get data relevant to the user from the data base
-    2. arrange data to be delivered to the template
-    3. render template with the data
-    """
-
-    return render(request, "TamnunMainPage/zik_view.html")
-
+    with open("TamnunMainPage\ZikDemo\data.json",'r') as datafile:
+        data = json.loads(datafile.read())
+    return render(request, "TamnunMainPage/zik_view.html", context=data)
 
 def display_main_page(request):
     """This view renders the Configuration builder view,
@@ -61,38 +29,3 @@ def display_main_page(request):
     context = ViewQueries.get_frontend_data(tms="11-11-11")
     return render(request, "TamnunMainPage/UAV.html", context)
 
-
-@ensure_csrf_cookie
-def calculation_endpoint(request):
-    """This is an API endpoint. Activation of the endpoint with a POST request will trigger a configuraion sequence.
-
-    Arguments:
-        request {WSGI request} -- Djagno's request object. Must be a POST request and in JSON format,
-        in accordance to agreed upon keys (See docs for example)
-
-    Returns:
-        CalcResult [JSON]      -- Result of calculation: Fuel limitations and centrograms
-    """
-    error_json = {
-        "takeoff_fuel": "Error",
-        "landing_fuel": "Error",
-        "units": "Error",
-        "centrogram": [],
-    }
-    if request.method == "POST":
-        print("Got calculation request")
-        try:
-            data = json.loads(request.body)
-        except json.JSONDecodeError as error:
-            print(f"Error decoding client's request: {error}")
-            return JsonResponse(error_json)
-        except TypeError as error:
-            print(f"Error in request format: {error}")
-            return JsonResponse(error_json)
-        time.sleep(2)
-        print("done sleeping")
-        print(data)
-        CalcResult = json.dumps(WBCalc.get_limits(data))
-        # return JsonResponse(CalcResult)
-        return JsonResponse({"status": "Success"})
-    return HttpResponse("No Calculation request was made")
